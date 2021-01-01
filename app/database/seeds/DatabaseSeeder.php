9f43d73b6321c97c326271bbac726b18815aac38
おーもと <vitamin29290209@gmail.com>
2021-01-01 22:54:13 +0900

    mineテーブル作成

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(StockTableSeeder::class); 
+        $this->call(StockTableSeeder::class);
+        $this->call(MineTableSeeder::class);
 
     }
 }
