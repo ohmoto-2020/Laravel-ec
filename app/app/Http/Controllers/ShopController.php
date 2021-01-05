c7c1b54bf05602afd7345d7d8722e488e0398430
おーもと <vitamin29290209@gmail.com>
2021-01-06 08:45:32 +0900

    カート内表示

--- app/app/Http/Controllers/ShopController.php
+++ app/app/Http/Controllers/ShopController.php
@@ -15,30 +15,21 @@ class ShopController extends Controller
         return view('shop',compact('stocks'));
     }
 
-    public function myCart()
+    public function myCart(Cart $cart)
     {
-        $my_carts = Cart::all();
+        $my_carts = $cart->showCart();
         return view('mycart',compact('my_carts'));
-
     }
 
-    public function addMycart(Request $request)
+    public function addMycart(Request $request,Cart $cart)
     {
-        $user_id = Auth::id();
+        //カートに追加の処理
         $stock_id=$request->stock_id;
+        $message = $cart->addCart($stock_id);
 
-        $cart_add_info=Cart::firstOrCreate(['stock_id' => $stock_id,'user_id' => $user_id]);
-
-        if($cart_add_info->wasRecentlyCreated){
-            $message = 'カートに追加しました';
-        }
-        else{
-            $message = 'カートに登録済みです';
-        }
-
-        $my_carts = Cart::where('user_id',$user_id)->get();
+        //追加後の情報を取得
+        $my_carts = $cart->showCart();
 
         return view('mycart',compact('my_carts' , 'message'));
-
     }
 }
